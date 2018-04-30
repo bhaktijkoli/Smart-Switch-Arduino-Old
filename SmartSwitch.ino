7d2277eef64a32cac753ebf2ab69a790744fb18a
Bhaktij Koli <bhaktijkoli121@gmail.com>
2018-04-30 17:53:33 +0530

    Updated

--- SmartSwitch.ino
+++ SmartSwitch.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
@@ -9,14 +10,16 @@ void updateStates();
 String formatStatus();
 
 int TOTAL = 4;
-int DELAY = 2000;
+int DELAY = 1000;
 
 ESP8266WiFiMulti WiFiMulti;
+ESP8266WebServer server(80);
 WebSocketsServer webSocket = WebSocketsServer(81);
 
-int buttons[] = {D5, 56, D7, D8};
+int buttons[] = {D5, D6, D7, D8};
 int leds[] = {D1, D2, D3, D4};
 int states[] = {LOW, LOW, LOW, LOW};
+int ap_state = 0;
 
 void setup() {
   for(int i=0;i<TOTAL;i++) {
@@ -31,32 +34,31 @@ void setup() {
   Serial.println("Started");
   WiFiMulti.addAP("Bhk", "12345678");
   Serial.print("Connecting");
+  int count = 0;
   while(WiFiMulti.run() != WL_CONNECTED) {
-    delay(100);
+    delay(300);
     Serial.print(".");
+    if(count++ == 10) break;
   }
-  while(!MDNS.begin("touchswitch")) {
-    delay(100);
-  }
-  MDNS.addService("ws", "tcp", 81);
-  Serial.println("DNS Started");
-  webSocket.begin();
-  webSocket.onEvent(webSocketEvent);
-  Serial.printf("Web Socket Server started\n");
+  startWebServer();
+  startWebSocket();
+  Serial.print("AP IP:");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
   webSocket.loop();
+  server.handleClient();
   updateStates();
-  return;
+  if(WiFi.status() != WL_CONNECTED && ap_state == 0) startAccessPoint(); 
 
-  if(digitalRead(buttons[0]) == HIGH && digitalRead(buttons[1]) == HIGH) {
+  if(digitalRead(buttons[1]) == HIGH && digitalRead(buttons[2]) == HIGH) {
     setAll(HIGH);
     Serial.printf("All Off\n");
     webSocket.broadcastTXT(formatStatus().c_str());
     delay(DELAY);
   }
-  if(digitalRead(buttons[2]) == HIGH && digitalRead(buttons[3]) == HIGH) {
+  if(digitalRead(buttons[0]) == HIGH && digitalRead(buttons[3]) == HIGH) {
     setAll(LOW);
     Serial.printf("All On\n");
     webSocket.broadcastTXT(formatStatus().c_str());
@@ -109,6 +111,29 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length
   }
 }
 /*
+ * Start / Stop Services
+ */
+ void startWebSocket() {
+  while(!MDNS.begin("touchswitch")) {
+    delay(100);
+  }
+  MDNS.addService("ws", "tcp", 81);
+  Serial.println("DNS Started");
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+  Serial.printf("Web Socket Server started\n");
+ }
+ void startWebServer() {
+  server.on("/", handleRoot);
+  server.on("/scan", handleScan);
+  server.on("/connect", handleConnect);
+  server.begin();
+ }
+ void startAccessPoint() {
+    Serial.println("Configuring access point...");
+    ap_state = WiFi.softAP("touchswitch21");
+ }
+/*
 * Custom Functions
 */
 void setAll(int value) {
@@ -144,5 +169,51 @@ String formatStatus() {
 
   data += "}}";
   return data;
+}
+/*
+ * Web Server Handlers
+ */
+void handleRoot() {
+  server.send(200, "text/plain", "1"); 
+}
+void handleScan() {
+  Serial.println("Request for scan");
+  String data = "{\"networks\":[";
+  int n = WiFi.scanNetworks();
+  for (int i = 0; i < n; ++i) {
+      data += "{\"name\":\"";
+      data += WiFi.SSID(i);
+      data += "\",\"encryption\":\"";
+      data += WiFi.encryptionType(i);
+      data += "\"}";
+      if(i!=n-1) data += ",";
+    }
+   data  += "]}";
+   Serial.println(data);
+   server.send(200, "text/plain", data);
+}
+ void handleConnect() {
+  Serial.println("Request for connect");
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(server.arg("plain"));
+  String ssid = root["name"];
+  String password = root["password"];
+  WiFiMulti.addAP(ssid.c_str(), password.c_str());
+  Serial.print("Connecting");
+  int count = 0;
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(300);
+    if(count++ == 10) break;
+    Serial.print(".");
+  }
+  if(count >= 10) {
+    server.send(200, "text/plain", "0");
+    Serial.println("Fail connecting");
+  } else {
+    Serial.println("Connected.");
+    server.send(200, "text/plain", "1");
+    WiFi.mode(WIFI_STA);
+    ap_state = 0;
+  }
 }
 
